eee3c471e7ac84c5667c2aad3352f989d6472e5a
Nate Kidwell <nate@ludicast.com>
2010-09-27 19:00:33 -0400

    applied patch from JustinA1

--- lib/yaml_db.rb
+++ lib/yaml_db.rb
@@ -65,4 +65,11 @@ module YamlDb
     end
   end
 
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load File.expand_path('../../tasks/yaml_db_tasks.rake', 
+__FILE__)
+    end
+  end
+
 end
