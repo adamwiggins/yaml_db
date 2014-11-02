mauriciopasquier/yamldb

Update to RSpec 3

spec/serialization_helper_dump_spec.rb
@@ -3,60 +3,60 @@ require File.dirname(__FILE__) + '/base'
 describe SerializationHelper::Dump do
 
   before do
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:tables).and_return([ 'mytable', 'schema_info', 'schema_migrations' ])
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([ mock('a', :name => 'a', :type => :string), mock('b', :name => 'b', :type => :string) ])
-    ActiveRecord::Base.connection.stub!(:select_one).and_return({"count"=>"2"})
-    ActiveRecord::Base.connection.stub!(:select_all).and_return([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:tables).and_return([ 'mytable', 'schema_info', 'schema_migrations' ])
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([ double('a', :name => 'a', :type => :string), double('b', :name => 'b', :type => :string) ])
+    allow(ActiveRecord::Base.connection).to receive(:select_one).and_return({"count"=>"2"})
+    allow(ActiveRecord::Base.connection).to receive(:select_all).and_return([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
   end
 
   before(:each) do
-    File.stub!(:new).with('dump.yml', 'w').and_return(StringIO.new)
+    allow(File).to receive(:new).with('dump.yml', 'w').and_return(StringIO.new)
     @io = StringIO.new
   end
 
   it "should return a list of column names" do
-    SerializationHelper::Dump.table_column_names('mytable').should == [ 'a', 'b' ]
+    expect(SerializationHelper::Dump.table_column_names('mytable')).to eq([ 'a', 'b' ])
   end
 
   it "should return a list of tables without the rails schema table" do
-    SerializationHelper::Dump.tables.should == ['mytable']
+    expect(SerializationHelper::Dump.tables).to eq(['mytable'])
   end
 
   it "should return the total number of records in a table" do
-    SerializationHelper::Dump.table_record_count('mytable').should == 2
+    expect(SerializationHelper::Dump.table_record_count('mytable')).to eq(2)
   end
 
   it "should return all records from the database and return them when there is only 1 page" do
     SerializationHelper::Dump.each_table_page('mytable') do |records|
-      records.should == [ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ]
+      expect(records).to eq([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
     end
   end
 
   it "should paginate records from the database and return them" do
-    ActiveRecord::Base.connection.stub!(:select_all).and_return([ { 'a' => 1, 'b' => 2 } ], [ { 'a' => 3, 'b' => 4 } ])
+    allow(ActiveRecord::Base.connection).to receive(:select_all).and_return([ { 'a' => 1, 'b' => 2 } ], [ { 'a' => 3, 'b' => 4 } ])
 
     records = [ ]
     SerializationHelper::Dump.each_table_page('mytable', 1) do |page|
-      page.size.should == 1
+      expect(page.size).to eq(1)
       records.concat(page)
     end
 
-    records.should == [ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ]
+    expect(records).to eq([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
   end
 
   it "should dump a table's contents to yaml" do
-    SerializationHelper::Dump.should_receive(:dump_table_columns)
-    SerializationHelper::Dump.should_receive(:dump_table_records)
+    expect(SerializationHelper::Dump).to receive(:dump_table_columns)
+    expect(SerializationHelper::Dump).to receive(:dump_table_records)
     SerializationHelper::Dump.dump_table(@io, 'mytable')
   end
 
   it "should not dump a table's contents when the record count is zero" do
-    SerializationHelper::Dump.stub!(:table_record_count).with('mytable').and_return(0)
-    SerializationHelper::Dump.should_not_receive(:dump_table_columns)
-    SerializationHelper::Dump.should_not_receive(:dump_table_records)
+    allow(SerializationHelper::Dump).to receive(:table_record_count).with('mytable').and_return(0)
+    expect(SerializationHelper::Dump).to_not receive(:dump_table_columns)
+    expect(SerializationHelper::Dump).to_not receive(:dump_table_records)
     SerializationHelper::Dump.dump_table(@io, 'mytable')
   end
 

spec/serialization_helper_load_spec.rb
@@ -3,74 +3,74 @@ require File.dirname(__FILE__) + '/base'
 describe SerializationHelper::Load do
 
   before do
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
 
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:transaction).and_yield
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:transaction).and_yield
     @io = StringIO.new
   end
 
   it "should truncate the table" do
-    ActiveRecord::Base.connection.stub!(:execute).with("TRUNCATE mytable").and_return(true)
-    ActiveRecord::Base.connection.should_not_receive(:execute).with("DELETE FROM mytable")
+    allow(ActiveRecord::Base.connection).to receive(:execute).with("TRUNCATE mytable").and_return(true)
+    expect(ActiveRecord::Base.connection).to_not receive(:execute).with("DELETE FROM mytable")
     SerializationHelper::Load.truncate_table('mytable')
   end
 
   it "should delete the table if truncate throws an exception" do
-    ActiveRecord::Base.connection.should_receive(:execute).with("TRUNCATE mytable").and_raise()
-    ActiveRecord::Base.connection.should_receive(:execute).with("DELETE FROM mytable").and_return(true)
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("TRUNCATE mytable").and_raise()
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("DELETE FROM mytable").and_return(true)
     SerializationHelper::Load.truncate_table('mytable')
   end
 
   it "should call reset pk sequence if the connection adapter is postgres" do
-    ActiveRecord::Base.connection.should_receive(:respond_to?).with(:reset_pk_sequence!).and_return(true)
-    ActiveRecord::Base.connection.should_receive(:reset_pk_sequence!).with('mytable')
+    expect(ActiveRecord::Base.connection).to receive(:respond_to?).with(:reset_pk_sequence!).and_return(true)
+    expect(ActiveRecord::Base.connection).to receive(:reset_pk_sequence!).with('mytable')
     SerializationHelper::Load.reset_pk_sequence!('mytable')
   end
 
   it "should not call reset pk sequence for other adapters" do
-    ActiveRecord::Base.connection.should_receive(:respond_to?).with(:reset_pk_sequence!).and_return(false)
-    ActiveRecord::Base.connection.should_not_receive(:reset_pk_sequence!)
+    expect(ActiveRecord::Base.connection).to receive(:respond_to?).with(:reset_pk_sequence!).and_return(false)
+    expect(ActiveRecord::Base.connection).to_not receive(:reset_pk_sequence!)
     SerializationHelper::Load.reset_pk_sequence!('mytable')
   end
 
   it "should insert records into a table" do
-    mca = mock('a',:name => 'a')
-    mcb = mock('b', :name => 'b')
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([mca , mcb ])
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('a').and_return('a')
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('b').and_return('b')
-    ActiveRecord::Base.connection.stub!(:quote).with(1, mca).and_return("'1'")
-    ActiveRecord::Base.connection.stub!(:quote).with(2, mcb).and_return("'2'")
-    ActiveRecord::Base.connection.stub!(:quote).with(3, mca).and_return("'3'")
-    ActiveRecord::Base.connection.stub!(:quote).with(4, mcb).and_return("'4'")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('1','2')")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('3','4')")
+    mca = double('a',:name => 'a')
+    mcb = double('b', :name => 'b')
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([mca , mcb ])
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('a').and_return('a')
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('b').and_return('b')
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(1, mca).and_return("'1'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(2, mcb).and_return("'2'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(3, mca).and_return("'3'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(4, mcb).and_return("'4'")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('1','2')")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('3','4')")
 
     SerializationHelper::Load.load_records('mytable', ['a', 'b'], [[1, 2], [3, 4]])
   end
 
   it "should quote column names that correspond to sql keywords" do
-    mca = mock('a',:name => 'a')
-    mccount = mock('count', :name => 'count')
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([mca , mccount ])
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('a').and_return('a')
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('count').and_return('"count"')
-    ActiveRecord::Base.connection.stub!(:quote).with(1, mca).and_return("'1'")
-    ActiveRecord::Base.connection.stub!(:quote).with(2, mccount).and_return("'2'")
-    ActiveRecord::Base.connection.stub!(:quote).with(3, mca).and_return("'3'")
-    ActiveRecord::Base.connection.stub!(:quote).with(4, mccount).and_return("'4'")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('1','2')")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('3','4')")
+    mca = double('a',:name => 'a')
+    mccount = double('count', :name => 'count')
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([mca , mccount ])
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('a').and_return('a')
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('count').and_return('"count"')
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(1, mca).and_return("'1'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(2, mccount).and_return("'2'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(3, mca).and_return("'3'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(4, mccount).and_return("'4'")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('1','2')")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('3','4')")
 
     SerializationHelper::Load.load_records('mytable', ['a', 'count'], [[1, 2], [3, 4]])
   end
 
   it "should truncate the table and then load the records into the table" do
-    SerializationHelper::Load.should_receive(:truncate_table).with('mytable')
-    SerializationHelper::Load.should_receive(:load_records).with('mytable', ['a', 'b'], [[1, 2], [3, 4]])
-    SerializationHelper::Load.should_receive(:reset_pk_sequence!).with('mytable')
+    expect(SerializationHelper::Load).to receive(:truncate_table).with('mytable')
+    expect(SerializationHelper::Load).to receive(:load_records).with('mytable', ['a', 'b'], [[1, 2], [3, 4]])
+    expect(SerializationHelper::Load).to receive(:reset_pk_sequence!).with('mytable')
 
     SerializationHelper::Load.load_table('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] })
   end

spec/yaml_load_spec.rb
@@ -4,11 +4,11 @@ require 'active_support/core_ext/kernel/debugger'
 describe YamlDb::Load do
 
   before do
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
 
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:transaction).and_yield
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:transaction).and_yield
   end
 
   before(:each) do
@@ -16,17 +16,17 @@ describe YamlDb::Load do
   end
 
   it "should call load structure for each document in the file" do
-    YAML.should_receive(:load_documents).with(@io).and_yield({ 'mytable' => {
+    expect(YAML).to receive(:load_documents).with(@io).and_yield({ 'mytable' => {
           'columns' => [ 'a', 'b' ],
           'records' => [[1, 2], [3, 4]]
         } } )
-    YamlDb::Load.should_receive(:load_table).with('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] },true)
+    expect(YamlDb::Load).to receive(:load_table).with('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] },true)
     YamlDb::Load.load(@io)
   end
 
   it "should not call load structure when the document in the file contains no records" do
-    YAML.should_receive(:load_documents).with(@io).and_yield({ 'mytable' => nil })
-    YamlDb::Load.should_not_receive(:load_table)
+    expect(YAML).to receive(:load_documents).with(@io).and_yield({ 'mytable' => nil })
+    expect(YamlDb::Load).to_not receive(:load_table)
     YamlDb::Load.load(@io)
   end
 

adamwiggins/yaml_db

spec/serialization_helper_dump_spec.rb
@@ -3,60 +3,60 @@ require File.dirname(__FILE__) + '/base'
 describe SerializationHelper::Dump do
 
   before do
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:tables).and_return([ 'mytable', 'schema_info', 'schema_migrations' ])
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([ mock('a', :name => 'a', :type => :string), mock('b', :name => 'b', :type => :string) ])
-    ActiveRecord::Base.connection.stub!(:select_one).and_return({"count"=>"2"})
-    ActiveRecord::Base.connection.stub!(:select_all).and_return([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:tables).and_return([ 'mytable', 'schema_info', 'schema_migrations' ])
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([ double('a', :name => 'a', :type => :string), double('b', :name => 'b', :type => :string) ])
+    allow(ActiveRecord::Base.connection).to receive(:select_one).and_return({"count"=>"2"})
+    allow(ActiveRecord::Base.connection).to receive(:select_all).and_return([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
   end
 
   before(:each) do
-    File.stub!(:new).with('dump.yml', 'w').and_return(StringIO.new)
+    allow(File).to receive(:new).with('dump.yml', 'w').and_return(StringIO.new)
     @io = StringIO.new
   end
 
   it "should return a list of column names" do
-    SerializationHelper::Dump.table_column_names('mytable').should == [ 'a', 'b' ]
+    expect(SerializationHelper::Dump.table_column_names('mytable')).to eq([ 'a', 'b' ])
   end
 
   it "should return a list of tables without the rails schema table" do
-    SerializationHelper::Dump.tables.should == ['mytable']
+    expect(SerializationHelper::Dump.tables).to eq(['mytable'])
   end
 
   it "should return the total number of records in a table" do
-    SerializationHelper::Dump.table_record_count('mytable').should == 2
+    expect(SerializationHelper::Dump.table_record_count('mytable')).to eq(2)
   end
 
   it "should return all records from the database and return them when there is only 1 page" do
     SerializationHelper::Dump.each_table_page('mytable') do |records|
-      records.should == [ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ]
+      expect(records).to eq([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
     end
   end
 
   it "should paginate records from the database and return them" do
-    ActiveRecord::Base.connection.stub!(:select_all).and_return([ { 'a' => 1, 'b' => 2 } ], [ { 'a' => 3, 'b' => 4 } ])
+    allow(ActiveRecord::Base.connection).to receive(:select_all).and_return([ { 'a' => 1, 'b' => 2 } ], [ { 'a' => 3, 'b' => 4 } ])
 
     records = [ ]
     SerializationHelper::Dump.each_table_page('mytable', 1) do |page|
-      page.size.should == 1
+      expect(page.size).to eq(1)
       records.concat(page)
     end
 
-    records.should == [ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ]
+    expect(records).to eq([ { 'a' => 1, 'b' => 2 }, { 'a' => 3, 'b' => 4 } ])
   end
 
   it "should dump a table's contents to yaml" do
-    SerializationHelper::Dump.should_receive(:dump_table_columns)
-    SerializationHelper::Dump.should_receive(:dump_table_records)
+    expect(SerializationHelper::Dump).to receive(:dump_table_columns)
+    expect(SerializationHelper::Dump).to receive(:dump_table_records)
     SerializationHelper::Dump.dump_table(@io, 'mytable')
   end
 
   it "should not dump a table's contents when the record count is zero" do
-    SerializationHelper::Dump.stub!(:table_record_count).with('mytable').and_return(0)
-    SerializationHelper::Dump.should_not_receive(:dump_table_columns)
-    SerializationHelper::Dump.should_not_receive(:dump_table_records)
+    allow(SerializationHelper::Dump).to receive(:table_record_count).with('mytable').and_return(0)
+    expect(SerializationHelper::Dump).not_to receive(:dump_table_columns)
+    expect(SerializationHelper::Dump).not_to receive(:dump_table_records)
     SerializationHelper::Dump.dump_table(@io, 'mytable')
   end
 

spec/serialization_helper_load_spec.rb
@@ -3,74 +3,74 @@ require File.dirname(__FILE__) + '/base'
 describe SerializationHelper::Load do
 
   before do
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
 
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:transaction).and_yield
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:transaction).and_yield
     @io = StringIO.new
   end
 
   it "should truncate the table" do
-    ActiveRecord::Base.connection.stub!(:execute).with("TRUNCATE mytable").and_return(true)
-    ActiveRecord::Base.connection.should_not_receive(:execute).with("DELETE FROM mytable")
+    allow(ActiveRecord::Base.connection).to receive(:execute).with("TRUNCATE mytable").and_return(true)
+    expect(ActiveRecord::Base.connection).not_to receive(:execute).with("DELETE FROM mytable")
     SerializationHelper::Load.truncate_table('mytable')
   end
 
   it "should delete the table if truncate throws an exception" do
-    ActiveRecord::Base.connection.should_receive(:execute).with("TRUNCATE mytable").and_raise()
-    ActiveRecord::Base.connection.should_receive(:execute).with("DELETE FROM mytable").and_return(true)
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("TRUNCATE mytable").and_raise()
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("DELETE FROM mytable").and_return(true)
     SerializationHelper::Load.truncate_table('mytable')
   end
 
   it "should call reset pk sequence if the connection adapter is postgres" do
-    ActiveRecord::Base.connection.should_receive(:respond_to?).with(:reset_pk_sequence!).and_return(true)
-    ActiveRecord::Base.connection.should_receive(:reset_pk_sequence!).with('mytable')
+    expect(ActiveRecord::Base.connection).to receive(:respond_to?).with(:reset_pk_sequence!).and_return(true)
+    expect(ActiveRecord::Base.connection).to receive(:reset_pk_sequence!).with('mytable')
     SerializationHelper::Load.reset_pk_sequence!('mytable')
   end
 
   it "should not call reset pk sequence for other adapters" do
-    ActiveRecord::Base.connection.should_receive(:respond_to?).with(:reset_pk_sequence!).and_return(false)
-    ActiveRecord::Base.connection.should_not_receive(:reset_pk_sequence!)
+    expect(ActiveRecord::Base.connection).to receive(:respond_to?).with(:reset_pk_sequence!).and_return(false)
+    expect(ActiveRecord::Base.connection).not_to receive(:reset_pk_sequence!)
     SerializationHelper::Load.reset_pk_sequence!('mytable')
   end
 
   it "should insert records into a table" do
-    mca = mock('a',:name => 'a')
-    mcb = mock('b', :name => 'b')
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([mca , mcb ])
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('a').and_return('a')
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('b').and_return('b')
-    ActiveRecord::Base.connection.stub!(:quote).with(1, mca).and_return("'1'")
-    ActiveRecord::Base.connection.stub!(:quote).with(2, mcb).and_return("'2'")
-    ActiveRecord::Base.connection.stub!(:quote).with(3, mca).and_return("'3'")
-    ActiveRecord::Base.connection.stub!(:quote).with(4, mcb).and_return("'4'")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('1','2')")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('3','4')")
+    mca = double('a',:name => 'a')
+    mcb = double('b', :name => 'b')
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([mca , mcb ])
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('a').and_return('a')
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('b').and_return('b')
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(1, mca).and_return("'1'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(2, mcb).and_return("'2'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(3, mca).and_return("'3'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(4, mcb).and_return("'4'")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('1','2')")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,b) VALUES ('3','4')")
 
     SerializationHelper::Load.load_records('mytable', ['a', 'b'], [[1, 2], [3, 4]])
   end
 
   it "should quote column names that correspond to sql keywords" do
-    mca = mock('a',:name => 'a')
-    mccount = mock('count', :name => 'count')
-    ActiveRecord::Base.connection.stub!(:columns).with('mytable').and_return([mca , mccount ])
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('a').and_return('a')
-    ActiveRecord::Base.connection.stub!(:quote_column_name).with('count').and_return('"count"')
-    ActiveRecord::Base.connection.stub!(:quote).with(1, mca).and_return("'1'")
-    ActiveRecord::Base.connection.stub!(:quote).with(2, mccount).and_return("'2'")
-    ActiveRecord::Base.connection.stub!(:quote).with(3, mca).and_return("'3'")
-    ActiveRecord::Base.connection.stub!(:quote).with(4, mccount).and_return("'4'")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('1','2')")
-    ActiveRecord::Base.connection.should_receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('3','4')")
+    mca = double('a',:name => 'a')
+    mccount = double('count', :name => 'count')
+    allow(ActiveRecord::Base.connection).to receive(:columns).with('mytable').and_return([mca , mccount ])
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('a').and_return('a')
+    allow(ActiveRecord::Base.connection).to receive(:quote_column_name).with('count').and_return('"count"')
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(1, mca).and_return("'1'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(2, mccount).and_return("'2'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(3, mca).and_return("'3'")
+    allow(ActiveRecord::Base.connection).to receive(:quote).with(4, mccount).and_return("'4'")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('1','2')")
+    expect(ActiveRecord::Base.connection).to receive(:execute).with("INSERT INTO mytable (a,\"count\") VALUES ('3','4')")
 
     SerializationHelper::Load.load_records('mytable', ['a', 'count'], [[1, 2], [3, 4]])
   end
 
   it "should truncate the table and then load the records into the table" do
-    SerializationHelper::Load.should_receive(:truncate_table).with('mytable')
-    SerializationHelper::Load.should_receive(:load_records).with('mytable', ['a', 'b'], [[1, 2], [3, 4]])
-    SerializationHelper::Load.should_receive(:reset_pk_sequence!).with('mytable')
+    expect(SerializationHelper::Load).to receive(:truncate_table).with('mytable')
+    expect(SerializationHelper::Load).to receive(:load_records).with('mytable', ['a', 'b'], [[1, 2], [3, 4]])
+    expect(SerializationHelper::Load).to receive(:reset_pk_sequence!).with('mytable')
 
     SerializationHelper::Load.load_table('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] })
   end

spec/yaml_load_spec.rb
@@ -4,11 +4,11 @@ require 'active_support/core_ext/kernel/debugger'
 describe YamlDb::Load do
 
   before do
-    SerializationHelper::Utils.stub!(:quote_table).with('mytable').and_return('mytable')
+    allow(SerializationHelper::Utils).to receive(:quote_table).with('mytable').and_return('mytable')
 
-    silence_warnings { ActiveRecord::Base = mock('ActiveRecord::Base', :null_object => true) }
-    ActiveRecord::Base.stub(:connection).and_return(stub('connection').as_null_object)
-    ActiveRecord::Base.connection.stub!(:transaction).and_yield
+    silence_warnings { ActiveRecord::Base = double('ActiveRecord::Base').as_null_object }
+    allow(ActiveRecord::Base).to receive(:connection).and_return(double('connection').as_null_object)
+    allow(ActiveRecord::Base.connection).to receive(:transaction).and_yield
   end
 
   before(:each) do
@@ -16,17 +16,17 @@ describe YamlDb::Load do
   end
 
   it "should call load structure for each document in the file" do
-    YAML.should_receive(:load_documents).with(@io).and_yield({ 'mytable' => {
+    expect(YAML).to receive(:load_documents).with(@io).and_yield({ 'mytable' => {
           'columns' => [ 'a', 'b' ],
           'records' => [[1, 2], [3, 4]]
         } } )
-    YamlDb::Load.should_receive(:load_table).with('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] },true)
+    expect(YamlDb::Load).to receive(:load_table).with('mytable', { 'columns' => [ 'a', 'b' ], 'records' => [[1, 2], [3, 4]] },true)
     YamlDb::Load.load(@io)
   end
 
   it "should not call load structure when the document in the file contains no records" do
-    YAML.should_receive(:load_documents).with(@io).and_yield({ 'mytable' => nil })
-    YamlDb::Load.should_not_receive(:load_table)
+    expect(YAML).to receive(:load_documents).with(@io).and_yield({ 'mytable' => nil })
+    expect(YamlDb::Load).not_to receive(:load_table)
     YamlDb::Load.load(@io)
   end
 